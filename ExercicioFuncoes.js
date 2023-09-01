a03178e5d12e6771fe657f3fa3273c3170f076be
RicksonPassos <rpc2307@gmail.com>
2023-08-31 21:30:36 -0300

    Exercicio Calculadora metodos/objetos

--- ExercicioFuncoes.js
+++ ExercicioFuncoes.js
@@ -11,15 +11,6 @@ const calculadora = {
     dividir: (n1, n2) => {
         return n1/n2;
     },
-    fatorial: (n) => {
-        let calc = [];
-        while(n > 0){
-            calc.push(n);
-            n--;
-        }
-        let fator = calc.join("*");
-        console.log(`!${n} = ${fator}`);
-    }
+    
 }
 
-calculadora.fatorial(5);
